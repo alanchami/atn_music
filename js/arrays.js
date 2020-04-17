8a7d3e2a2c5e40b2f2414a124bcd40a99ef0d9d9
hijos <achami@udesa.edu.ar>
2020-04-17 18:19:00 -0300

    cambio

--- js/arrays.js
+++ js/arrays.js
@@ -1,5 +1,6 @@
-let participantes=['consu','mateo','nico','milton'];
-participantes[4]='Raul';
-console.log(participantes);
-let numerosorteados=['1','6','9','39']
-console.log(numerosorteados)
+document.addEventListener('DOMContentLoaded',()=>{
+const elementosCarousel = document.querySelectorAll('.carousel');
+M.Carousel.init(elementosCarousel, {
+    duration:150
+} );
+});
