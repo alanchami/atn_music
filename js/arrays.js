df23f8d8afa4631dcc7bfc060ed7a51bf309afb2
hijos <achami@udesa.edu.ar>
2020-05-08 14:23:11 -0300

    JS

--- js/arrays.js
+++ js/arrays.js
@@ -1,12 +1,95 @@
-function saludar(nombre){
-    let saludoGeneral = 'Bienvenido' + nombre;
-    console.log(saludoGeneral);
-    
+let nombre = prompt('¿como te llamas?');
+
+let retorno = saludar(nombre);
+
+let huno = document.querySelector('.hola')
+
+console.log(huno);
+
+
+huno.innerHTML = retorno;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let carcel= prompt ('eres culpable o no');
+
+if(carcel == 'si'){
+    alert('Iras a la carcel');
+}
+else if(carcel == 'no'){
+    alert('iras a casa bro');
 }
 
-var miVar = 'Alan';
+let numero1 = 1
+let numero2 = 2
+let resultado = (numero1 == numero2);
+    alert(resultado);
+if(numero1 > 12 && numero1 < 20) {
+    console.log('El numero es mayor a 12 y menor a 20')
+}
+else{
+    console.log('el numero no es mayor a 12 ni menor a 20');
+}
+*/
 
-let nombre = 'Alan';
 
-saludar(miVar);
-saludar(nombre)
+
+
+
+
+
+
+
+
+
+
+/*
+let retorno = saludar(nombre);
+
+console.log(retorno);
+
+//alert('hola soy un alert');
+
+let valor = confirm('quieres ingresar en esta pagina');
+
+console.log(valor);
+
+
+if(valor == true) {
+   console.log('si es verdadero');
+}
+
+let numero= prompt('dame un numero');
+if (numero > 0){
+    console.log('es positivo');
+} else if (numero < 0){
+    console.log('es negativo');
+} else{
+    console.log('es cero');
+}
+
+console.log('no es un numero');
+*/
+
+
+
+
+
+
+
+
