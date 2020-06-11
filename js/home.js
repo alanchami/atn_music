1a3aff10dac62e3e7dab73c0db4c447f549a03cb
alanchami <63103816+alanchami@users.noreply.github.com>
2020-06-11 13:23:02 -0300

    cambio

--- js/home.js
+++ js/home.js
@@ -45,7 +45,30 @@ for (const album of albums) {
     `
 }
 
+
+
+
+
 })
 .catch(function(error){
     console.error(error)
 })
+
+var bounceIn = anime({
+    targets: '.letter__part',
+    translateY: {
+                  value: [-30, 0],
+                  duration: 900,
+          delay: 1000,
+                  elasticity: 600,
+                  easing: 'easeOutElastic'
+              },
+              opacity: {
+                  value: [0, 1],
+                  duration: 300,
+                  easing: 'linear',
+          delay: 1000
+              }
+    });
+
+  
