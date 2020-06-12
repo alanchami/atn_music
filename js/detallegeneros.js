487df40a7669cda09e8e057e88bf0744a7b5ce0b
nicomizrahi1 <nmizrahi@udesa.edu.ar>
2020-06-12 11:20:46 -0300

    cambios

--- js/detallegeneros.js
+++ js/detallegeneros.js
@@ -12,21 +12,18 @@ fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre/"+genero
 .then(function(datos){
 
     let artistaGenero = datos.data
-    let contenedorArtistas = document.querySelector(".artistageneros")
+    let contenedorArtistas = document.querySelector(".grid-container")
     
     for (const artista of artistaGenero) {
         contenedorArtistas.innerHTML += 
         `
-        <div>
-            <div class="container2">
-                <a href="Yatra.html?id=${artista.id}">
-                    <img class="imagen-drake" src="${artista.picture_big}" alt="Imagen de ${artista.name}">
-                </a>
-                <a class="textos" href="details.html?id=${artista.id}">${artista.name}</a>
-                <div class="textos2">${artista.type}</div>
-            </div>
-        </div>
-        
+       
+        <div class="genero"> 
+          <a href="yatra.html?id=${artista.id}">
+         <img class="imagen" src="${artista.picture_big}" alt=""> </a>
+         <h4>${artista.name}</h4>
+        </div> 
+
         `
     }
     
