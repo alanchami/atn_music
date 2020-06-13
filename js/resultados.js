ba777ad60fe9102136f72566feb9a64cb237add5
hijos <achami@udesa.edu.ar>
2020-06-13 19:10:56 -0300

    cambio

--- js/resultados.js
+++ js/resultados.js
@@ -19,10 +19,9 @@ fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q='+bus
       for (const resultado of tracks) {
          console.log(resultado);
          
-        nombre.innerHTML += `<a href="playa.html?id=${resultado.title}"><img class="inolvidable" src="${resultado.album.cover_xl}" alt=""><h3>${resultado.artist.name}</h3><h3>${resultado.album.title}</h3></a>`
+        nombre.innerHTML += `<a href="detalles-track.html?id=${resultado.id}<h4>${resultado.title}"></h4><img class="inolvidable" src="${resultado.album.cover_xl}" alt=""><h3>${resultado.artist.name}</h3><h3>${resultado.album.title}</h3></a>`
         
       }  
-        h3.style.TextTransform = "uppercase"
       
     })
     .catch(function(error){
--- js/tracks.js
+++ js/tracks.js
@@ -25,6 +25,11 @@ contenedorData.innerHTML =`
 <h4>${track.artist.name} </h3>
 <h4> Album: ${track.album.title}</h3>
 <h4>${track.duration}Seg</h4>
+<br>
+<br>
+<div> 
+  <button class="btn btn-primary" id="agregar"> + Agregar a tu Playlist </button>
+</div>
 </div>
     <audio  align="center" src="${track.preview}" controls="controls" type="audio/mpeg" preload="preload">
     </audio>
